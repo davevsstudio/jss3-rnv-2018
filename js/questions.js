a5aafdd70cc9de959c6be9da7f54d8ea38dc46a0
davestar <142616037+davevsstudio@users.noreply.github.com>
2024-09-12 22:27:57 -0700

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,301 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "The Nigeria flag was designed by",
+    answer: "tawio akinwunmi",
+    options: [
+      "tawio akinwunmi",
+      "tawio akinkunmi",
+      "tawio adewunmi",
+      "tawio olakunmi"
+    ]
+  },
+    {
+    numb: 2,
+    question: "The National Youth Service Corps (NYSC) was established in",
+    answer: "1973",
+    options: [
+      "1973",
+      "1974",
+      "1975",
+      "1976"
+    ]
+  },
+    {
+    numb: 3,
+    question: "A citizen is ______ member of a country",
+    answer: "loyal",
+    options: [
+      "legal",
+      "loyal",
+      "good",
+      "truthful"
+    ]
+  },
+    {
+    numb: 4,
+    question: "A document that contains the rules and regulation govering a country is",
+    answer: "constitution",
+    options: [
+      "convention",
+      "constitution",
+      "national symbol",
+      "national identity"
+    ]
+  },
+    {
+    numb: 5,
+    question: "The current Nigeria constitution is in",
+    answer: "encouraging the exploitation of consumers",
+    options: [
+      "1979",
+      "1989",
+      "1999",
+      "2009"
+    ]
+  },
+  {
+    numb: 6,
+    question: "There are __________ geo-political zones in Nigeria",
+    answer: "6",
+    options: [
+      "5",
+      "6",
+      "7",
+      "4"
+    ]
+  },
+
+  {
+    numb: 7,
+    question: "The Nigeria passport is issued by", 
+    answer: "ministry of external affairs",
+    options: [
+      "ministry of internal affairs",
+      "ministry of external affairs",
+      "customs service",
+      "state security service"
+    ]
+  },
+
+  {
+    numb: 8,
+    question: "Where did decomcracy originate from?",
+    answer: "greece",
+    options: [
+      "america",
+      "france",
+      "greece",
+      "britain"
+    ]
+  },
+  {
+    numb: 9,
+    question: "All these are negative bahviours except",
+    answer: "fresh water",
+    options: [
+      "cultism",
+      "drug abuse",
+      "honesty",
+      "corruption"
+    ]
+  },
+
+  {
+    numb: 10,
+    question: "A good citizen pay his/her ________ regularly",
+    answer: "taxes",
+    options: [
+      "money",
+      "bill",
+      "fine",
+      "taxes"
+    ]
+  },
+
+  {
+    numb: 11,
+    question: "Which of this this animal serve as means of transportation",
+    answer: "donkey",
+    options: [
+      "pig",
+      "goat",
+      "donkey",
+      "sheep"
+    ]
+  },
+
+  {
+    numb: 12,
+    question: "The following are farm structure EXCEPT",
+    answer: "weighbridge",
+    options: [
+      "silo",
+      "crib",
+      "rhumbus",
+      "weighbridge"
+    ]
+  },
+
+
+  {
+    numb: 13,
+    question: "The reproduction part of plant is",
+    answer: "flower",
+    options: [
+      "leaf",
+      "seed",
+      "stem",
+      "flower"
+    ]
+  },
+
+  {
+    numb: 14,
+    question: "It has parallel vein leaf, fibrous root, these are the characterictics of ________ plant",
+    answer: "monocotyledon",
+    options: [
+      "monocotyledon",
+      "dicotyledon",
+      "cocoa",
+      "orange"
+    ]
+  },
+
+  {
+    numb: 15,
+    question: "Crocodiles, alligators and turtles are example of aqutic",
+    answer: "reptiles",
+    options: [
+      "reptiles",
+      "mammals",
+      "amphibians",
+      "arthropod"
+    ]
+  },
+
+  {
+    numb: 16,
+    question: "A conical or cylindral  plastic sheet used to hold water in poultry is called",
+    answer: "water through",
+    options: [
+      "feeding through",
+      "water through",
+      "container",
+      "poultry through"
+    ]
+  },
+
+
+  {
+    numb: 17,
+    question: "The removal of surplus seeding from areas sown with seed is _______",
+    answer: "thinning",
+    options: [
+      "mulching",
+      "supplying",
+      "thinning",
+      "harvesting"
+    ]
+  },
+
+  {
+    numb: 18,
+    question: "All these are agricultural product EXCEPT",
+    answer: "plastic",
+    options: [
+      "kenaf",
+      "cotten",
+      "rubber",
+      "plastic"
+    ]
+  },
+
+  {
+    numb: 19,
+    question: "Trypanosome is transmitted by a vector",
+    answer: "tsetse fly",
+    options: [
+      "tsetse fly",
+      "white fly",
+      "house fly",
+      "aphids"
+    ]
+  },
+
+
+
+  {
+    numb: 20,
+    question: "Which of the following is energy feed concentrate",
+    answer: "maize",
+    options: [
+      "pigeon pea",
+      "maize",
+      "groundnut cake",
+      "soya bean meal"
+    ]
+  },
+
+
+  {
+    numb: 21,
+    question: "Stalking is a pratice which is beneficial to one of the following crops",
+    answer: "yam and pumpkin",
+    options: [
+      "yam and cocoyam",
+      "rice and wheat",
+      "yam and pumpkin",
+      "tobacco and tomato"
+    ]
+  },
+
+  {
+    numb: 22,
+    question: "The following are aquatic animals except",
+    answer: "crocodile and snakes",
+    options: [
+      "fish and prawns",
+      "lobsters and crayfish",
+      "oysters and periwinkles",
+      "crocodile and snakes"
+    ]
+  },
+
+  {
+    numb: 23,
+    question: "When product are in direct contact with packaging, it is a _______ packaging  ",
+    answer: "primary",
+    options: [
+      "primary",
+      "secondary",
+      "tertiary",
+      "universal"
+    ]
+  },
+
+  {
+    numb: 24,
+    question: "Causative organism of anthrax disease is",
+    answer: "bacteria",
+    options: [
+      "fungi",
+      "bacteria",
+      "virus",
+      "protozoa"
+    ]
+  },
+
+  {
+    numb: 25,
+    question: "The priice of agricultural product will be affected by the following EXCEPT",
+    answer: "advertisement of product",
+    options: [
+      "transportation cost",
+      "seasonality cost",
+      "advertisement of product",
+      "perish ability of the product"
+    ]
+  },
+];
